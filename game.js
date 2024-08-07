ec5743f6098e15c8edfe9c584c6d0eb40933ead8
Damian Smythe <developmentoctal@gmail.com>
2024-08-07 15:50:15 +0200

    Update game.js

--- game.js
+++ game.js
@@ -6,16 +6,16 @@ var Snake = (function () {
   var intervalID;
   
   var tileCount = 10;
-  var gridSize = 400 / tileCount;
+  var gridSize = 400/tileCount;
 
   const INITIAL_PLAYER = { x: Math.floor(tileCount / 2), y: Math.floor(tileCount / 2) };
   
-  var velocity = { x: 0, y: 0 };
+  var velocity = { x:0, y:0 };
   var player = { x: INITIAL_PLAYER.x, y: INITIAL_PLAYER.y };
 
   var walls = false;
 
-  var fruit = { x: 1, y: 1 };
+  var fruit = { x:1, y:1 };
 
   var trail = [];
   var tail = INITIAL_TAIL;
@@ -24,11 +24,11 @@ var Snake = (function () {
   var points = 0;
   var pointsMax = 0;
 
-  var ActionEnum = { 'none': 0, 'up': 1, 'down': 2, 'left': 3, 'right': 4 };
+  var ActionEnum = { 'none':0, 'up':1, 'down':2, 'left':3, 'right':4 };
   Object.freeze(ActionEnum);
   var lastAction = ActionEnum.none;
   
-  function setup() {
+  function setup () {
     canv = document.getElementById('gc');
     ctx = canv.getContext('2d');
     // document.addEventListener('keydown', keyPush);
@@ -61,25 +61,25 @@ var Snake = (function () {
 
     action: {
       up: function () {
-        if (lastAction != ActionEnum.down) {
+        if (lastAction != ActionEnum.down){
           velocity.x = 0;
           velocity.y = -1;
         }
       },
       down: function () {
-        if (lastAction != ActionEnum.up) {
+        if (lastAction != ActionEnum.up){
           velocity.x = 0;
           velocity.y = 1;
         }
       },
       left: function () {
-        if (lastAction != ActionEnum.right) {
+        if (lastAction != ActionEnum.right){
           velocity.x = -1;
           velocity.y = 0;
         }
       },
       right: function () {
-        if (lastAction != ActionEnum.left) {
+        if (lastAction != ActionEnum.left){
           velocity.x = 1;
           velocity.y = 0;
         }
@@ -87,10 +87,11 @@ var Snake = (function () {
     },
 
     RandomFruit: function () {
-      if (walls) {
-        fruit.x = 1 + Math.floor(Math.random() * (tileCount - 2));
-        fruit.y = 1 + Math.floor(Math.random() * (tileCount - 2));
-      } else {
+      if(walls){
+        fruit.x = 1+Math.floor(Math.random() * (tileCount-2));
+        fruit.y = 1+Math.floor(Math.random() * (tileCount-2));
+      }
+      else {
         fruit.x = Math.floor(Math.random() * tileCount);
         fruit.y = Math.floor(Math.random() * tileCount);
       }
@@ -106,9 +107,23 @@ var Snake = (function () {
 
       reward = -0.1;
 
-      function HitWall() {
-        // Reset game if the player hits the wall
-        game.reset();
+      function DontHitWall () {
+        if(player.x < 0) player.x = tileCount-1;
+        if(player.x >= tileCount) player.x = 0;
+        if(player.y < 0) player.y = tileCount-1;
+        if(player.y >= tileCount) player.y = 0;
+      }
+      function HitWall () {
+        if(player.x < 1) game.reset();
+        if(player.x > tileCount-2) game.reset();
+        if(player.y < 1) game.reset();
+        if(player.y > tileCount-2) game.reset();
+
+        ctx.fillStyle = 'grey';
+        ctx.fillRect(0,0,gridSize-1,canv.height);
+        ctx.fillRect(0,0,canv.width,gridSize-1);
+        ctx.fillRect(canv.width-gridSize+1,0,gridSize,canv.height);
+        ctx.fillRect(0, canv.height-gridSize+1,canv.width,gridSize);
       }
 
       var stopped = velocity.x == 0 && velocity.y == 0;
@@ -122,24 +137,19 @@ var Snake = (function () {
       if (velocity.x == 1 && velocity.y == 0) lastAction = ActionEnum.right;
       
       ctx.fillStyle = 'rgba(40,40,40,0.8)';
-      ctx.fillRect(0, 0, canv.width, canv.height);
+      ctx.fillRect(0,0,canv.width,canv.height);
       
-      if (walls) HitWall();
-      else {
-        // Check for wall collision in the case where walls are not present
-        if (player.x < 0 || player.x >= tileCount || player.y < 0 || player.y >= tileCount) {
-          HitWall();
-        }
-      }
+      if(walls) HitWall();
+      else DontHitWall();
       
       // game.log();
       
-      if (!stopped) {
-        trail.push({ x: player.x, y: player.y });
-        while (trail.length > tail) trail.shift();
+      if (!stopped){
+        trail.push({x:player.x, y:player.y});
+        while(trail.length > tail) trail.shift();
       }
 
-      if (!stopped) {
+      if(!stopped) {
         ctx.fillStyle = 'rgba(200,200,200,0.2)';
         ctx.font = "small-caps 14px Helvetica";
         ctx.fillText("(esc) reset", 24, 356);
@@ -147,26 +157,26 @@ var Snake = (function () {
       }
       
       ctx.fillStyle = 'green';
-      for (var i = 0; i < trail.length - 1; i++) {
-        ctx.fillRect(trail[i].x * gridSize + 1, trail[i].y * gridSize + 1, gridSize - 2, gridSize - 2);
+      for(var i=0; i<trail.length-1; i++) {
+        ctx.fillRect(trail[i].x * gridSize+1, trail[i].y * gridSize+1, gridSize-2, gridSize-2);
         
         // console.debug(i + ' => player:' + player.x, player.y + ', trail:' + trail[i].x, trail[i].y);
-        if (!stopped && trail[i].x == player.x && trail[i].y == player.y) {
+        if (!stopped && trail[i].x == player.x && trail[i].y == player.y){
           game.reset();
         }
         ctx.fillStyle = 'lime';
       }
-      ctx.fillRect(trail[trail.length - 1].x * gridSize + 1, trail[trail.length - 1].y * gridSize + 1, gridSize - 2, gridSize - 2);
+      ctx.fillRect(trail[trail.length-1].x * gridSize+1, trail[trail.length-1].y * gridSize+1, gridSize-2, gridSize-2);
       
       if (player.x == fruit.x && player.y == fruit.y) {
-        if (!fixedTail) tail++;
+        if(!fixedTail) tail++;
         points++;
-        if (points > pointsMax) pointsMax = points;
+        if(points > pointsMax) pointsMax = points;
         reward = 1;
         game.RandomFruit();
         // make sure new fruit didn't spawn in snake tail 
-        while ((function () {
-          for (var i = 0; i < trail.length; i++) {
+        while((function () {
+          for(var i=0; i<trail.length; i++) {
             if (trail[i].x == fruit.x && trail[i].y == fruit.y) {
               game.RandomFruit();
               return true;
@@ -177,9 +187,9 @@ var Snake = (function () {
       }
       
       ctx.fillStyle = 'red';
-      ctx.fillRect(fruit.x * gridSize + 1, fruit.y * gridSize + 1, gridSize - 2, gridSize - 2);
+      ctx.fillRect(fruit.x * gridSize+1, fruit.y * gridSize+1, gridSize-2, gridSize-2);
       
-      if (stopped) {
+      if(stopped) {
         ctx.fillStyle = 'rgba(250,250,250,0.8)';
         ctx.font = "small-caps bold 14px Helvetica";
         ctx.fillText("press ARROW KEYS to START...", 24, 374);
@@ -194,37 +204,37 @@ var Snake = (function () {
     }
   }
   
-  function keyPush(evt) {
-    switch (evt.keyCode) {
+  function keyPush (evt) {
+    switch(evt.keyCode) {
       case 37: //left
-        game.action.left();
-        evt.preventDefault();
-        break;
-        
+      game.action.left();
+      evt.preventDefault();
+      break;
+      
       case 38: //up
-        game.action.up();
-        evt.preventDefault();
-        break;
-        
+      game.action.up();
+      evt.preventDefault();
+      break;
+      
       case 39: //right
-        game.action.right();
-        evt.preventDefault();
-        break;
-        
+      game.action.right();
+      evt.preventDefault();
+      break;
+      
       case 40: //down
-        game.action.down();
-        evt.preventDefault();
-        break;
-        
+      game.action.down();
+      evt.preventDefault();
+      break;
+      
       case 32: //space
-        Snake.pause();
-        evt.preventDefault();
-        break;
-        
+      Snake.pause();
+      evt.preventDefault();
+      break;
+      
       case 27: //esc
-        game.reset();
-        evt.preventDefault();
-        break;
+      game.reset();
+      evt.preventDefault();
+      break;
     }
   }
 
@@ -251,7 +261,8 @@ var Snake = (function () {
       keyboard: function (state) {
         if (state) {
           document.addEventListener('keydown', keyPush);
-        } else {
+        }
+        else {
           document.removeEventListener('keydown', keyPush);
         }
       },
@@ -268,7 +279,7 @@ var Snake = (function () {
     },
 
     action: function (act) {
-      switch (act) {
+      switch(act) {
         case 'left':
           game.action.left();
           break;
@@ -296,7 +307,7 @@ var Snake = (function () {
       pointsMax = 0;
     },
 
-    data: {
+    data:  {
       player: player,
       fruit: fruit,
       trail: function () {
