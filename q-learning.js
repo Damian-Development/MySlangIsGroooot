6295cfa4b9a2869a607ef32ce418597caab867a8
Damian Smythe <developmentoctal@gmail.com>
2024-08-07 15:48:18 +0200

    Update q-learning.js with cookie support

--- q-learning.js
+++ q-learning.js
@@ -163,6 +163,32 @@ var QLearning = (function () {
     previousAction = action;
   }
 
+  function saveToCookies() {
+    // Save qTable, score, and missed to cookies
+    setCookie('qTable', JSON.stringify(qTable), 7); // Save for 7 days
+    setCookie('score', score, 7);
+    setCookie('missed', missed, 7);
+  }
+
+  function loadFromCookies() {
+    // Load qTable, score, and missed from cookies
+    var savedQTable = getCookie('qTable');
+    if (savedQTable) qTable = JSON.parse(savedQTable);
+
+    var savedScore = getCookie('score');
+    if (savedScore) score = parseInt(savedScore, 10);
+
+    var savedMissed = getCookie('missed');
+    if (savedMissed) missed = parseInt(savedMissed, 10);
+  }
+
+  function deleteCookies() {
+    // Delete cookies
+    eraseCookie('qTable');
+    eraseCookie('score');
+    eraseCookie('missed');
+  }
+
   return {
     run: function () {
       clearInterval(intervalID);
@@ -191,6 +217,7 @@ var QLearning = (function () {
       qTable = {};
       score = 0;
       missed = 0;
+      deleteCookies(); // Delete cookies when resetting
     },
 
     changeConst: {
@@ -237,7 +264,42 @@ var QLearning = (function () {
       import: function (newQ) {
         qTable = newQ;
       }
+    },
+
+    save: function () {
+      saveToCookies();
+    },
+
+    load: function () {
+      loadFromCookies();
     }
   }
 
 })();
+
+// Cookie helper functions
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {
+  document.cookie = name + '=; Max-Age=-99999999;';
+}
